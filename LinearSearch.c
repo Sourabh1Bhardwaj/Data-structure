7e287661112e952b324930f3d04305ccab902903
Sourabh bhardwaj <72859333+Sourabh1Bhardwaj@users.noreply.github.com>
2020-10-15 02:08:02 +0530

    Add files via upload

---
+++ LinearSearch.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,search,i,e=0;
+    printf("enter the number of element ");
+    scanf("%d",&n);
+    
+    printf("Enter the array element");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        printf("position of %d element of %d=\n",i,arr[i]);
+    }
+    
+    printf("Enter element you want to search ");
+    scanf("%d",&search);
+    for(i=0;i<n;i++)
+    {
+        if(search == arr[i])
+        {
+            printf("Element search %dnd index ",i);
+            e=1;
+        }
+    }
+    if(e==0)
+    {
+        printf("Element not found in the array");
+    }
+    return 0;
+}
